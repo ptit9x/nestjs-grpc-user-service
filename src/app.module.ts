34b14f24c8560a147288e7e660446f92ca72cba8
huynhdn <huynhdn@vmodev.com>
2022-10-14 16:14:41 +0700

    add authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [    
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      database: 'micro_auth',
+      username: 'kevin',
+      password: null,
+      entities: ['dist/**/*.entity.{ts,js}'],
+      synchronize: true, // never true in production!
+    }),
+    AuthModule,
+    UserModule
+],
 })
 export class AppModule {}
